946a86c749eb410c0625056d95be1879782837a8
João Vitor <joaovitorgs2002@gmail.com>
2024-09-02 10:11:23 -0300

    Mais exercicios

---
+++ Desenvolvimento WEB/Curso Javascript/10 - Repetiçõse/exercicio.js
@@ -0,0 +1,23 @@
+function enquanto() {
+    var c = 1;
+    while (c <= 8) {
+        console.log(`tudo bem? ${c}`);
+        c++;
+    }
+}
+
+enquanto();
+
+function faca_enquanto() {
+    var c = 1;
+    do {
+        console.log(`Tudo bem? ${c}`);
+        c++;
+    } while (c <= 8);
+}
+
+faca_enquanto();
+
+function para() {
+    for(c=1){}
+}
