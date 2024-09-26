fdb707741a893caf524cf07499c3a11c259b763f
João Vitor <joaovitorgs2002@gmail.com>
2024-09-26 09:49:13 -0300

    Exercicios da matéria de lógica

---
+++ Desenvolvimento WEB/Curso Javascript/13 - Switch/script.js
@@ -0,0 +1,17 @@
+let escolha = 3;
+let resposta;
+
+switch (escolha) {
+    case 1:
+        resposta = 5;
+        console.log(resposta);
+        break;
+    case 2:
+        resposta = 6;
+        console.log(resposta);
+        break;
+    case 3:
+        resposta = 7;
+        console.log(resposta);
+        break;
+}
