661660bb9ba57f6da41121a3e75e7fd25d3db1c1
João Vitor <joaovitorgs2002@gmail.com>
2024-10-24 10:32:58 -0300

    Fim do design

---
+++ Calculadora/script.js
@@ -0,0 +1,19 @@
+function calcularMais(valor1, valor2) {
+    const resultado = valor1 + valor2;
+    return resultado;
+}
+
+function calcularMenos(valor1, valor2) {
+    const resultado = valor1 - valor2;
+    return resultado;
+}
+
+function calcularMult(valor1, valor2) {
+    const resultado = valor1 * valor2;
+    return resultado;
+}
+
+function calcularDiv(valor1, valor2) {
+    const resultado = valor1 / valor2;
+    return resultado;
+}
