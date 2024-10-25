ad0fff6a6ad08aa1d176541ff024916eb8e85a44
João Vitor <joaovitorgs2002@gmail.com>
2024-10-25 08:38:52 -0300

    Fim da calculadora

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -2,4 +2,11 @@ function adicionar(numero) {
     let resultado = (document.getElementById("resultado").innerHTML += numero);
 }
 
-function limpar() {}
+function limpar() {
+    let resultado = (document.getElementById("resultado").innerHTML = ``);
+}
+
+function calcular() {
+    let resultado = document.getElementById("resultado");
+    resultado.innerText = eval(resultado.innerText);
+}
