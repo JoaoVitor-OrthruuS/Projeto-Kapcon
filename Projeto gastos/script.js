1b94109115f5b95ca5e40fdc14e80f9bbabbd4a9
João Vitor <joaovitorgs2002@gmail.com>
2025-01-07 11:12:11 -0300

    Projeto controle de gastos

--- Projeto gastos/script.js
+++ Projeto gastos/script.js
@@ -1,13 +1,58 @@
 const btnAddGasto = document.getElementById("btnAddGasto");
+let total = 0;
 
 btnAddGasto.addEventListener("click", () => {
-    const data = document.getElementById("data").value;
-    const descricao = document.getElementById("descricao").value;
-    const categoria = document.getElementById("categoria").value;
-    const valor = document.getElementById("valor").value;
-    const formaPagamento = document.getElementById("formaPagamento").value;
+    //Entrada de todos os dados de gastos
+    const dados = [
+        document.getElementById("data").value,
+        document.getElementById("descricao").value,
+        document.getElementById("categoria").value,
+        document.getElementById("valor").value,
+        document.getElementById("formaPagamento").value,
+    ];
 
-    const saida = document.getElementById("saida");
+    //Testa se todos os campos foram preenchidos.
+    if (
+        dados[0] === "" ||
+        dados[1] === "" ||
+        dados[2] === "" ||
+        dados[3] === "" ||
+        dados[4] === ""
+    ) {
+        console.log("Um dos campos está Vazio!");
+    } else {
+        //Saida do valor total de gastos
+        const valorGasto = parseFloat(dados[3]);
+        const valorTotal = document.getElementById("valorTotal");
 
-    saida.innerHTML += `<tr><td>${data}</td><td>${descricao}</td><td>${categoria}</td><td>${valor}</td><td>${formaPagamento}</td></tr>`;
+        total += valorGasto;
+        valorTotal.innerText = `R$ ${total.toFixed(2)}`;
+
+        //Declarando a saida, o tbody que recebe os appends
+        const saida = document.getElementById("saida");
+
+        //criando as novas trs para inserir as tds
+        const novoTr = document.createElement("tr");
+
+        //append da nova tr para no tbody
+        saida.appendChild(novoTr);
+
+        //Criando os novos campos na tabela preenchidos
+        dados.forEach((dados) => {
+            const novoTd = document.createElement("td");
+            novoTd.innerText = dados;
+            novoTr.appendChild(novoTd);
+        });
+
+        //btnDeletar para remover as linhas da tabela
+        const btnDeletar = document.createElement("button");
+        btnDeletar.innerText = "excluir";
+        btnDeletar.classList = "btnDeletar";
+
+        novoTr.appendChild(btnDeletar);
+
+        btnDeletar.addEventListener("click", () => {
+            saida.removeChild(novoTr);
+        });
+    }
 });
